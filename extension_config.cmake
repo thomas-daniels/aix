5cf85b5950ce2e3f366fcdde077f31e1f88245d4
Thomas Daniels <daniels.thomas@pm.me>
2026-02-13 19:41:37 +0100

    Try both Windows builds and one Linux

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,8 +1,14 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
 set (CMAKE_CXX_STANDARD 17)
-add_compile_definitions(_HAS_STD_BYTE=0)
-add_compile_definitions(WIN32_LEAN_AND_MEAN)
+
+if (WIN32)
+  if (MINGW)
+    add_compile_definitions(WIN32_LEAN_AND_MEAN)
+  else ()
+    add_compile_definitions(_HAS_STD_BYTE=0)
+  endif()
+endif()
 
 # Extension from this repo
 duckdb_extension_load(aixchess
